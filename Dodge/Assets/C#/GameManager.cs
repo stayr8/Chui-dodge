9a4fcf6a5c4be4956fa4c3bc2e1b9062ade5c800
20181290 한찬희 <83389555+stayr8@users.noreply.github.com>
2021-05-17 11:23:51 +0900

    닷지 플레이어 1.0
    닷지 플레이어 1.0

---
+++ Dodge/Assets/C#/GameManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;// UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText; // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text recordText;// 최고 기록을 표시할 텍스트 컴포넌트
+
+    public GameObject level;
+    public GameObject bullerSpawnerPrefab;
+    private Vector3[] bulletSpawners = new Vector3[4];
+    int spawnCounter = 0;
+
+    private float surviveTime; // 생존시간
+    private bool isGameover; // 게임오버 상태
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+
+        bulletSpawners[0].x = -8f;
+        bulletSpawners[0].y = 1f;
+        bulletSpawners[0].z = 8f;
+
+        bulletSpawners[1].x = 8f;
+        bulletSpawners[1].y = 1f;
+        bulletSpawners[1].z = 8f;
+
+        bulletSpawners[2].x = 8f;
+        bulletSpawners[2].y = 1f;
+        bulletSpawners[2].z = -8f;
+
+        bulletSpawners[3].x = -8f;
+        bulletSpawners[3].y = 1f;
+        bulletSpawners[3].z = -8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //게임오버가 아닌 동안
+        if (!isGameover)
+        {
+            //생존시간 갱신
+            surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
+            timeText.text = "Time : " + (int)surviveTime;
+
+            if(surviveTime < 5f && spawnCounter == 0)
+            {
+                GameObject bulletSpawner = Instantiate(bullerSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnCounter++;
+            }
+            else if (surviveTime >= 5f && surviveTime < 10f && spawnCounter == 1)
+            {
+                GameObject bulletSpawner = Instantiate(bullerSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnCounter++;
+            }
+            else if (surviveTime >= 10f && surviveTime < 15f && spawnCounter == 2)
+            {
+                GameObject bulletSpawner = Instantiate(bullerSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnCounter++;
+            }
+            else if (surviveTime >= 15f && surviveTime < 20f && spawnCounter == 3)
+            {
+                GameObject bulletSpawner = Instantiate(bullerSpawnerPrefab, bulletSpawners[spawnCounter], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnCounter];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnCounter++;
+            }
+        }
+        else
+        {
+            //게임 오버 상태에서 R 키를 누른 경우
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        // 현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        // 게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
+
+        // BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        // 이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
+        if (surviveTime > bestTime)
+        {
+            //최고 기록 값을 현재 생존 시간 값으로 변경
+            bestTime = surviveTime;
+            //변경된 최고 기록을 BestTime 키로 저장
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        //최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+        recordText.text = "Best Time: " + (int)bestTime;
+    }
+
+   
+}
